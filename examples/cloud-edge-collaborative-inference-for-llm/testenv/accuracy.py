48b428d0b72079b209dda64fa74d81070b59deab
Pragnya <prag1704@gmail.com>
2026-05-25 12:52:42 +0530

    Guard LLM accuracy when no predictions
    Signed-off-by: Pragnya <prag1704@gmail.com>

--- examples/cloud-edge-collaborative-inference-for-llm/testenv/accuracy.py
+++ examples/cloud-edge-collaborative-inference-for-llm/testenv/accuracy.py
@@ -53,6 +53,9 @@ def acc(y_true, y_pred):
     # 使用列表推导来比较两个列表中的元素是否相同
     same_elements = [y_pred[i] == y_true[i] for i in range(len(y_pred))]
 
+    if not same_elements:
+        return 0.0
+
     # 计算相同元素的数量
     acc = sum(same_elements) / len(same_elements)
 
